2bef30ceae9feb5c29b3255d2141705791eb853b
Brus Palaj <brus.palaj71@gmail.com>
2020-04-03 18:21:05 -0400

    added functions for all js files, needs some tweaking as badges wont show up properly

--- index.js
+++ index.js
@@ -1,12 +1,73 @@
-const questions = [
+const fs = require("fs"); 
+const path = require("path"); 
+const inquirer = require("inquirer"); 
+const api = require("./utils/api"); 
+const generateMarkdown = ("./utils/generateMarkdown"); 
 
+
+const questions = [
+    {
+        type: "input", 
+        name: "github",
+        message: "What is your GitHub username?"
+    }, 
+    {
+        type: "input", 
+        name: "title",
+        message: "What is your projects name?"
+    }, 
+    {
+        type: "input", 
+        name: "description",
+        message: "Provide a description of your project?"
+    }, 
+    {
+        type: "input", 
+        name: "license",
+        message: "What type of license should your project have?", 
+        choices: ["MIT", "APACHE 2.O", "GPL.3.0", "BSD 3", "None"]
+    }, 
+    {
+        type: "input", 
+        name: "github",
+        message: "What is your GitHub username?"
+    }, 
+    {
+        type: "input", 
+        name: "installation",
+        message: "What command should be run to install dependencies", 
+        default: "npm i"
+    }, 
+    {
+        type: "input", 
+        name: "test",
+        message: "What command should be run to run tests", 
+        default: "npm test"
+    },  
+    {
+        type: "input", 
+        name: "usage",
+        message: "What does the user need to know about using this repo?"
+    }, 
+    {
+        type: "input", 
+        name: "contributing",
+        message: "What does user need to know about contributing to this repo?"
+    }
 ];
 
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data); 
 }
 
 function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log("Searching..."); 
 
+        api.getUser(inquirerResponses.github).then(({ data }) => {
+            writeToFile("README.md", generateMarkdown({ ...inquirerResponses, ...data})); 
+        })
+    })
 }
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,83 @@
+function generateProjectUrl(github, title) {
+  const caseTitle = title.toLowerCase().split("").join("-"); 
+  return `https://github.com/${github}/${caseTitle}`; 
+}
+
+function renderLicenseBadge(license, github, title) {
+  if (license !== "None") {
+    return `[![GitHub license](https://imgshields.io/badge/license-${license}-b)`
+  }
+  return ''
+}
+
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License 
+      
+This project is license under the ${license} license.`
+    )
+  }
+  return ''
+}
+
 function generateMarkdown(data) {
   return `
-# ${data.title}
+  # ${data.title}
+  ${renderLicenseBadge(data.license, data.github, data.title)}
+  
+  ## Description 
 
-`;
+  ${data.description}
+
+  ## Table of Contents 
+
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  *[License](#license)
+
+  *[Contributing](#contributing)
+
+  *[Tests](#tests)
+
+  *[Questions](#questions)
+
+  ## Installation
+
+  To install required dependencies, run this command: 
+
+  \'\'\'
+  ${data.installation}
+  \'\'\'
+
+  ## Usage 
+
+  ${data.usage}
+  
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing 
+
+  ${data.contributing}
+
+  ## Tests 
+
+  To run tests, run this command: 
+
+  \'\'\'
+  ${data.test}
+  \'\'\'
+
+  ## Questions 
+
+  <img src="${data.avatar_url}" 
+
+  Questions regarding this repo, reach out to [${data.github.user}]
+
+  `; 
 }
+
 
 module.exports = generateMarkdown;
